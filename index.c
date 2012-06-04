03675c7be2f8ee6ae5ad93a3f774127e7d942e1a
Svip <svip@diku.dk>
2012-06-04 22:05:48 +0200

    Now as the previous front page.

--- index.c
+++ index.c
@@ -9,9 +9,33 @@ int pagemain(int argc, char** argv) {
   
   webpage_start(&builder, NULL);
   
+  enter_tag(&builder, "article", NULL);
+  enter_tag(&builder, "header", NULL);
+  enter_tag(&builder, "h1", NULL);
+  enter_tag(&builder, "b", NULL);
+  insert_text(&builder, "topDatamat 0x0");
+  leave_tag(&builder); /* b */
+  insert_text(&builder, " udgivet! (");
+  enter_tag(&builder, "a", "href", "http://youtube.com/watch?v=0ohC89vJjnY", NULL);
+  insert_text(&builder, "Click here for English subtitles");
+  leave_tag(&builder); /* a */
+  insert_text(&builder, ")");
+  leave_tag(&builder); /* h1 */
+  leave_tag(&builder); /* header */
+  enter_tag(&builder, "div", "class", "post", NULL);
+  webpage_youtubeembed(&builder, "lgmA9z8Sb5E", 853, 480);
   enter_tag(&builder, "p", NULL);
-  insert_text(&builder, "Hej!");
-  leave_tag(&builder);
+  insert_text(&builder, "Så er første afsnit af topDatamat her endeligt!");
+  leave_tag(&builder); /* p */
+  enter_tag(&builder, "p", NULL);
+  insert_text(&builder, "Nyd det her eller ");
+  enter_tag(&builder, "a", "href", "http://youtube.com/watch?v=lgmA9z8Sb5E", NULL);
+  insert_text(&builder, "direkte på YouTube");
+  leave_tag(&builder); /* a */
+  insert_text(&builder, ".");
+  leave_tag(&builder); /* p */
+  leave_tag(&builder); /* div */
+  leave_tag(&builder); /* article */
   
   webpage_end(&builder);
 
