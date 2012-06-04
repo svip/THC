39ba37ef3bd58ae7c5c5f9b95ce9b880caa891eb
Svip <svip@diku.dk>
2012-06-04 21:36:58 +0200

    Now it can run with Apache2.

--- thc.c
+++ thc.c
@@ -1,9 +1,9 @@
 #include "thc.h"
 
 int main(int argc, char** argv) {
-  printf("HTTP/1.1 200 OK\r\n");
-  printf("Server: THC %s\r\n", THC_VERSION);
-  printf("Content-type: text/html\r\n");
+/*  printf("HTTP/1.1 200 OK\r\n");
+  printf("Server: THC %s\r\n", THC_VERSION);*/
+  printf("Content-type: text/html; charset=utf-8\r\n");
   printf("Cache-control: max-age=3600\r\n");
   printf("\r\n");
   return pagemain(argc, argv);
