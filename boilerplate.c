302458517dcffdb567d3fe8d7380d41ba216f022
Svip <svip@diku.dk>
2012-06-04 19:56:16 +0200

    Webservers require spacing between headers and content.

--- boilerplate.c
+++ boilerplate.c
@@ -4,8 +4,9 @@
 
 int main(int argc, char** argv) {
   printf("HTTP/1.1 200 OK\r\n");
-  printf("Server: THC "VERSION"\r\n");
+  printf("Server: THC \"VERSION\"\r\n");
   printf("Content-type: text/html\r\n");
   printf("Cache-control: max-age=3600\r\n");
+  printf("\r\n");
   return pagemain(argc, argv);
 }
