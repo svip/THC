a3790e3634fab17b626f2928a9068f5fffe8c461
Troels Henriksen <athas@sigkill.dk>
2012-06-05 14:24:31 +0200

    Treat warnings as errors.

--- config.mk
+++ config.mk
@@ -7,7 +7,7 @@ LIBS =
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -DVERSION=\"${VERSION}\"
-CFLAGS   = -ansi -pedantic -Wall -Wextra -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -ansi -pedantic -Wall -Wextra -Werror -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
