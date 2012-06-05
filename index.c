1132261354b1f265ed099239c2b5e9dc422e08fe
Svip <svip@diku.dk>
2012-06-05 16:36:32 +0200

    Better indenting.

--- index.c
+++ index.c
@@ -11,20 +11,20 @@ int pagemain(int argc, char** argv) {
   webpage_start(builder, "Front", NULL);
 
   TAG(("article"),
-      TAG(("header"),
-          TAG(("h1"),
-              TAG(("b"), TEXT("topDatamat 0x0"));
-              TEXT(" udgivet ! (");
-              TAG(("a", "href", "http://youtube.com/watch?v=0ohC89vJjnY"),
-                  TEXT("Click here for English subtitles"))
-              TEXT(")")));
-      TAG(("div", "class", "post"),
-          webpage_youtubeembed(builder, "lgmA9z8Sb5E", 853, 480);
-          TAG(("p"), TEXT("Så er første afsnit af topDatamat her endeligt!"));
-          TAG(("p"),
-              TEXT("Nyd det her eller ")
-              TAG(("a", "href", "http://youtube.com/watch?v=lgmA9z8Sb5E"), TEXT("direkte på YouTube"))
-              TEXT("."))))
+    TAG(("header"),
+      TAG(("h1"),
+        TAG(("b"), TEXT("topDatamat 0x0"));
+        TEXT(" udgivet ! (");
+        TAG(("a", "href", "http://youtube.com/watch?v=0ohC89vJjnY"),
+          TEXT("Click here for English subtitles"))
+        TEXT(")")));
+    TAG(("div", "class", "post"),
+      webpage_youtubeembed(builder, "lgmA9z8Sb5E", 853, 480);
+      TAG(("p"), TEXT("Så er første afsnit af topDatamat her endeligt!"));
+      TAG(("p"),
+        TEXT("Nyd det her eller ")
+        TAG(("a", "href", "http://youtube.com/watch?v=lgmA9z8Sb5E"), TEXT("direkte på YouTube"))
+        TEXT("."))))
 
   webpage_end(builder);
 
