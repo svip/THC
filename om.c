f233a112262a72f9257e72bbe777f1d2bca98215
Troels Henriksen <athas@sigkill.dk>
2012-06-06 16:08:20 +0200

    Prettier templates in om.c.

--- om.c
+++ om.c
@@ -3,41 +3,28 @@
 #include <stdio.h>
 
 int pagemain(int argc, char** argv) {
-  struct html_builder builder;
+  struct html_builder builderv;
+  struct html_builder *builder = &builderv;
   UNUSED(argc);
   UNUSED(argv);
-  
-  webpage_start(&builder, "About", "Om");
-  
-  enter_tag(&builder, "article", NULL);
-  enter_tag(&builder, "header", NULL);
-  enter_tag(&builder, "h1", NULL);
-  insert_text(&builder, "Om");
-  leave_tag(&builder); /* h1 */
-  enter_tag(&builder, "h6", NULL);
-  insert_text(&builder, "English below");
-  leave_tag(&builder); /* h6 */
-  leave_tag(&builder); /* header */
-  enter_tag(&builder, "article", NULL);
-  enter_tag(&builder, "p", NULL);
-  insert_text(&builder, "Direkte fra DIKUs kælder kommer topDatamat - tre akavede nørder der forsøger at give et retvisende billede af datalogien.  Følg med når Brainfuck udfører dybdeborende interviews, Troels besvarer datalogiens dybeste spørgsmål og den norske Truls forsøger på at integrere sig i en fremmed kultur.");
-  leave_tag(&builder); /* p */
-  leave_tag(&builder); /* article */
-  enter_tag(&builder, "article", NULL);
-  enter_tag(&builder, "header", NULL);
-  enter_tag(&builder, "h1", NULL);
-  insert_text(&builder, "About");
-  leave_tag(&builder); /* h1 */
-  leave_tag(&builder); /* header */
-  enter_tag(&builder, "p", NULL);
-  insert_text(&builder, "topDatamat is a Danish sketch show with computers and IT in focus.");
-  leave_tag(&builder); /* p */
-  leave_tag(&builder); /* article */
-  leave_tag(&builder); /* article */
-  
-  webpage_end(&builder);
 
-  print_tree(builder.top_node, 0);
+  webpage_start(builder, "About", "Om");
+
+  TAG(("article"),
+      TAG(("header"),
+          TAG(("h1"), TEXT("Om");)
+          TAG(("h6"),
+              TEXT("English below")))
+      TAG(("article"),
+          TAG(("p"),
+              TEXT(("Direkte fra DIKUs kælder kommer topDatamat - tre akavede nørder der forsøger at give et retvisende billede af datalogien.  Følg med når Brainfuck udfører dybdeborende interviews, Troels besvarer datalogiens dybeste spørgsmål og den norske Truls forsøger på at integrere sig i en fremmed kultur."))))
+      TAG(("header"), TAG(("h1"), TEXT("About")))
+      TAG(("p"),
+          TEXT("topDatamat is a Danish sketch show with computers and IT in focus.")))
+
+  webpage_end(builder);
+
+  print_tree(builder->top_node, 0);
 
   return 0;
 }
